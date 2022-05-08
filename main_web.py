48716447f07931f540537798b0bd8990617550dc
BerdichevskiyM <max.berdichevskiy@gmail.com>
2022-05-09 01:41:29 +0400

    beta 1

--- main_web.py
+++ main_web.py
@@ -3,11 +3,10 @@ from math import ceil
 from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from random import randint
 
 
 app = Flask(__name__)
-db_name = f'local_data#{randint(0, 100)}#.db'
+db_name = f'local_data.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -27,7 +26,7 @@ class Note(db.Model):
 def main():
     six_note_groups = []
     if os.path.isfile(db_name):
-        six_note_groups = Note.query.order_by(Note.date).all()
+        six_note_groups = Note.query.order_by(Note.date.desc()).all()
         six_l = ceil(len(six_note_groups) / 6)
         six_note_groups = [six_note_groups[i * 6:(i + 1) * 6] for i in range(six_l)]
     return render_template('main.html', note_groups=six_note_groups)
@@ -46,6 +45,29 @@ def add_note():
         return redirect('/')
     except:
         return 'Невозможно добавить заметку'
+
+
+@app.route('/delete_note/<int:id>', methods=['GET', 'POST'])
+def delete_note(id):
+    note = Note.query.get_or_404(id)
+    try:
+        db.session.delete(note)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Невозможно удалить заметку'
+
+
+@app.route('/find_note', methods=['GET', 'POST'])
+def find_note():
+    founded_notes = []
+    head = request.form['header']
+    if os.path.isfile(db_name):
+        if head:
+            founded_notes = Note.query.filter_by(header=head).order_by(Note.date.desc()).all()
+        six_l = ceil(len(founded_notes) / 6)
+        founded_notes = [founded_notes[i * 6:(i + 1) * 6] for i in range(six_l)]
+    return render_template('single_main.html', note_groups=founded_notes)
 
 
 if __name__ == '__main__':
