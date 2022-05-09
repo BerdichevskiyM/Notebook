356032c8829280e4319dafe756bf4749ea76912a
BerdichevskiyM <max.berdichevskiy@gmail.com>
2022-05-09 12:00:01 +0400

    release 1 without server support

--- main_web.py
+++ main_web.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)
 db_name = f'local_data.db'
+app.config['SECRET_KEY'] = '6a060b7c7$$95a212Gb5d01G742e_18888fa114395cD328b_213eA1125594079aaFVDf5SV5168fe43f2FDHHDac441e80bf'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.String(100), default=datetime.utcnow().date())
     header = db.Column(db.String(100), nullable=False)
     text = db.Column(db.Text, nullable=False)
 
@@ -40,8 +40,9 @@ def add_note():
     text = request.form['text']
     note = Note(header=head, text=text)
     try:
-        db.session.add(note)
-        db.session.commit()
+        if head and text:
+            db.session.add(note)
+            db.session.commit()
         return redirect('/')
     except:
         return 'Невозможно добавить заметку'
@@ -62,9 +63,17 @@ def delete_note(id):
 def find_note():
     founded_notes = []
     head = request.form['header']
+    date = request.form['date']
     if os.path.isfile(db_name):
-        if head:
-            founded_notes = Note.query.filter_by(header=head).order_by(Note.date.desc()).all()
+        if head and date:
+            founded_notes = Note.query.filter_by(header=head, date=date)
+        elif head:
+            founded_notes = Note.query.filter_by(header=head)
+        elif date:
+            founded_notes = Note.query.filter_by(date=date)
+        else:
+            return render_template('single_main.html', note_groups=founded_notes)
+        founded_notes = founded_notes.order_by(Note.date.desc()).all()
         six_l = ceil(len(founded_notes) / 6)
         founded_notes = [founded_notes[i * 6:(i + 1) * 6] for i in range(six_l)]
     return render_template('single_main.html', note_groups=founded_notes)
